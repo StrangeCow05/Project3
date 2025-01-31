4d8b2f53fcf0112daa035102b1975a993d2ba94a
StrangeCow05 <alex.miller@bsu.edu>
2025-01-31 18:11:39 +0000

    fuck

--- horse.cpp
+++ horse.cpp
@@ -10,14 +10,15 @@ int main(){
  int horses[] = {0,0,0,0,0};
  while(keepGoing){
   for (int i=0; i< 5 ;i++){
-   advance(i, horses );
    if (isWinner (i, horses)){
     keepGoing == false;
    }else {
     keepGoing == true;
    }//end if
    printLane(i,horses);
+   advance(i, horses);
   }//end for
+ 
  std::cout <<"Press Enter to Continue:";
  std::cin.ignore();
  std::cout <<horses[1] <<std::endl;
@@ -28,37 +29,34 @@ return 0;
 }//end main
 
 void advance(int horseNum, int* horses){
- for (int i = 0; i < 5; i++){
+ for (int i=0 ; i <5; i++){
   std::random_device rd;
   std::uniform_int_distribution<int> dist(0,1);
   int coin = dist(rd);
   if (coin == 1){
-   horses[i] ++ ;
+   horses [i] ++ ;
   }//end if
- }//end for
+ }
 }//end advance
 
 
 void printLane(int horseNum, int* horses){
- for (int i = 0; i < 4; i++){
-  for (int j =0; j < 14; j++){
-   if (i == horses[j]){
+ for (int i = 0; i < 14; i++){
+   if (i == horses[i]){
     std::cout <<horses[i] ;
    }else{
     std::cout <<"." ;
    }//end if
-  }
-  std::cout<<"k"<<std::endl;
- }//end for 
+ }//end for
+ std::cout<<"K"<<std::endl; 
 }//end printlane
 
 bool isWinner(int horseNum, int* horses){
- for (int i = 0; i < 5; i++){
-  if (horses[i] == 14){
+  if (horses == 14){
    return false;
   }else {
    return true;
   }//end if
- }//end for
+ 
  
 }//end isWinner
